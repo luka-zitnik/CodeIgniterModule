99e5d503fdc0b7e40d8db4dfefa2438e821621b7
Luka Zitnik <luka.zitnik@gmail.com>
2016-01-31 10:23:08 +0100

    make index configurable

--- CodeIgniter.php
+++ CodeIgniter.php
@@ -11,12 +11,16 @@ use Codeception\Lib\Connector\UniversalRunkit as UniversalRunkitConnector;
 class CodeIgniter extends Framework
 {
 
+  protected $config = [
+    'index' => 'public/index.php'
+  ];
+
   public function _initialize() {
   }
 
   public function _before(TestCase $test) {
     $this->client = new UniversalRunkitConnector();
-    $this->client->setIndex(__DIR__ . '/../../../../../../public/index.php');
+    $this->client->setIndex(\Codeception\Configuration::projectDir() . $this->config['index']);
   }
 
 }
