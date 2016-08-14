ad5bba3c892bd445f4669a26d34e8676fcb17ec6
Luka Zitnik <luka.zitnik@gmail.com>
2016-08-14 22:03:33 +0200

    finish debugging on travis

--- tests/functional/UserdataCept.php
+++ tests/functional/UserdataCept.php
@@ -3,6 +3,5 @@ $I = new FunctionalTester($scenario);
 $I->wantTo('write, then read userdata data from another HTTP request');
 $I->sendAjaxPostRequest('/userdata/setUserdata', ['awsome_actor' => 'Warren Beatty']);
 $I->sendAjaxGetRequest('/userdata/readUserdata');
-codecept_debug($I->grabResponse());
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(['awsome_actor' => 'Warren Beatty']);
